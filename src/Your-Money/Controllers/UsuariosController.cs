15ed6b2503bd6e07afe556df2b916ee2fac0f8cf
Leonardo Lima <leodmelo@hotmail.com>
2023-05-15 23:27:20 -0300

    Updates:  - Adequação Filtro pesquisa saldo  - Criação função saldo total da conta

--- src/Your-Money/Controllers/UsuariosController.cs
+++ src/Your-Money/Controllers/UsuariosController.cs
@@ -110,10 +110,15 @@ namespace Your_Money.Controllers
             var valorDespesas = lancamentos.Where(x => x.Tipo == Transacao.Despesa &&
                                                   x.Data.Year == ano &&
                                                   x.Data.Month == mes).Sum(x => x.Valor);
+            
+            var valorReceitasTotal = lancamentos.Where(x => x.Tipo == Transacao.Receita).Sum(x => x.Valor);
+            
+            var valorDespesasTotal = lancamentos.Where(x => x.Tipo == Transacao.Despesa).Sum(x => x.Valor);
 
             ViewBag.ValorReceitas = valorReceitas;
             ViewBag.ValorDespesas = valorDespesas;
             ViewBag.Saldo = valorReceitas - valorDespesas;
+            ViewBag.SaldoTotal = valorReceitasTotal - valorDespesasTotal;
 
             var usuarioDbContext = _context.Usuarios.Where(i => i.Email == userEmail);
             return View(await usuarioDbContext.ToListAsync());
